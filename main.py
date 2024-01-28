1b422dc0a3d4d9f81cf6b3dd495fc045fbe8dc73
calipsso <kamil.vizy@gmail.com>
2024-01-28 21:51:31 +0100

    Version 1

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
+class Queue:
+    def __init__(self):
+        self.prvky = ["kamil", "peter"]
+    def prazdny(self):
+        if (len(self.prvky)) == 0:
+            return "Prazdny"
+        else:
+            return "Nieje prazdny zvol volbu 4 pre podrobny vypis"
+    def pridaj_prvok(self):
+        prvok = input()
+        self.prvky.append(prvok)
+        return f"pridal si {prvok}"
+    def odober_prvok(self):
+        prvok = input()
+        self.prvky.remove(prvok) # pop ak chcem cislo
+        return f"odobral si {prvok}"
+    def vypis_prvkov(self):
+        return self.prvky
+        #for prvok in self.prvky:
+            #return prvok
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Menu:
+    @staticmethod
+    def Vyber_z_menu():
+        print("Vyber z menu:")
+        print("1. Zisti stav zoznamu")
+        print("2. Pridaj prvok")
+        print("3. Odober prvok")
+        print("4. Vypis prvkov")
+
+queue = Queue()
+#queue.vypis_prvkov()
+
+while True:
+    Menu.Vyber_z_menu()
+    while True:
+        try:
+            vyber = int(input("zadaj volbu: "))
+            break
+        except:
+            print("zadavaj len cisla")
+    if vyber == 1:
+        print(queue.prazdny())
+    if vyber == 2:
+        print(queue.pridaj_prvok())
+    if vyber == 3:
+        print(queue.odober_prvok())
+    if vyber == 4:
+        print(queue.vypis_prvkov())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+queue = Queue()
+print(queue.pridaj_prvok("Tester"))
+queue.prazdny()
+queue.odober_prvok("kamil")
+queue.vypis_prvkov()
